a942804ff2bdb289a37f796814f3bf3ab9732fb0
Josh <Josh.Gill@london.gov.uk>
2024-04-29 13:13:01 +0100

    Excel Output
    Have created script to produce the start of the Excel Output. Need to add in metadata and other changes but will do after meeting.
    
    Also some report outputs are missing but will be added shortly.

--- create_workbook.R
+++ create_workbook.R
@@ -5,5 +5,36 @@ library(openxlsx)
 #Created by Josh on X date
 wb <- createWorkbook()
 
+#List
+Worksheets <- c("TNO", "Domestic_Abuse", "Sexual_Offences","AWL_Knife_U25", "Homicide", "Robbery_Offences","Burglary_Offences")
 
-saveWorkbook(wb, "basics.xlsx", overwrite = TRUE)  ## save to working directory
+#Add worksheets to workbook
+addWorksheet(wb, "TNO")
+writeData(wb, "TNO", TNO)
+
+addWorksheet(wb, "Domestic Abuse")
+writeData(wb, "Domestic Abuse", Domestic_Abuse)
+
+addWorksheet(wb, "Sexual_Offences")
+writeData(wb,"Sexual_Offences", Sexual_Offences)
+
+addWorksheet(wb, "AWL_Knife_U25")
+writeData(wb, "AWL_Knife_U25", ND_Knife_Crime)
+
+addWorksheet(wb, "Homicide")
+writeData(wb, "Homicide", Homicide_Offences)
+
+addWorksheet(wb, "Robbery_Offences")
+writeData(wb,"Robbery_Offences", Personal_Robbery)
+
+addWorksheet(wb, "Burglary_Offences")
+writeData(wb, "Burglary_Offences", Burglary_Offences)
+
+addWorksheet(wb, "TfMV_Offences")
+writeData(wb,"TfMV_Offences", Motor_Vehicle_Theft)
+
+addWorksheet(wb, "Theft_Person")
+writeData(wb, "Theft_Person", Theft_Person)
+
+#Save the completed workbook to my working directory
+saveWorkbook(wb, "State_Of_London_Crime.xlsx", overwrite = TRUE)
